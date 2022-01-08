78f6d6e3365327edbabeb361fe27c355fb2756ed
Kanelle Lorent <kanelle.lorent@mail-ecv.fr>
2022-01-08 17:06:34 +0100

    init add header

---
+++ partials/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <img :src="$prismic.asLink(document.header_logo)" />
+      <a href="#">{{ $prismic.asText(document.header_abonnements) }}</a>
+      <a href="#">{{ $prismic.asText(document.header_accueil) }}</a>
+      <a href="#">{{ $prismic.asText(document.header_bonscadeaux) }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      document: {},
+    }
+  },
+  async fetch() {
+    this.document = (await this.$prismic.api.getSingle('header')).data
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+    div{
+        width: 500px;
+        height: 200px;
+        background-color: $secondary-color;
+    }
+</style>

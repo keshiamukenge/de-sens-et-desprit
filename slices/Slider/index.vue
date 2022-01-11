1f099b1fecd545d076bf7a3d98f6bd9f3a9585a3
keshiamukenge <mukenge.keshia@gmail.com>
2022-01-11 20:12:41 +0100

    feature: add fonctionnal slider and cursor

--- slices/Slider/index.vue
+++ slices/Slider/index.vue
@@ -1,16 +1,45 @@
 <template>
-  <section class="section--home-slider">
-    <prismic-image :field="slice.primary.sliderImg1" />
-    <prismic-image :field="slice.primary.sliderImg2" />
-    <prismic-image :field="slice.primary.sliderImg3" />
-    <prismic-image :field="slice.primary.sliderImg4" />
-    <prismic-image :field="slice.primary.sliderImg5" />
+  <section
+    ref="sectionSlider"
+    class="section--home-slider"
+    @mousemove="updateCoordinates($event)"
+    @mouseleave="disableCursor"
+    @mouseenter="enableCursor"
+  >
+    <div ref="cursor" class="cursor">
+      <span ref="cursorContent">Drag</span>
+    </div>
+    <swiper :options="swiperOptions">
+      <swiper-slide>
+        <prismic-image :field="slice.primary.sliderImg1" />
+      </swiper-slide>
+      <swiper-slide>
+        <prismic-image :field="slice.primary.sliderImg2" />
+      </swiper-slide>
+      <swiper-slide>
+        <prismic-image :field="slice.primary.sliderImg3" />
+      </swiper-slide>
+      <swiper-slide>
+        <prismic-image :field="slice.primary.sliderImg4" />
+      </swiper-slide>
+      <swiper-slide>
+        <prismic-image :field="slice.primary.sliderImg5" />
+      </swiper-slide>
+    </swiper>
   </section>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import GSAP from 'gsap'
+import { ref } from '@vue/reactivity'
+
 export default {
   name: 'Slider',
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
   props: {
     slice: {
       type: Object,
@@ -20,20 +49,103 @@ export default {
       },
     },
   },
+
+  data() {
+    return {
+      swiperOptions: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+      },
+      x: 0,
+      y: 0,
+      cursor: ref(null),
+      sectionSlider: ref(null),
+      cursorContent: ref(null),
+    }
+  },
+
+  methods: {
+    updateCoordinates(event) {
+      this.x = event.clientX
+      this.y = event.clientY
+      GSAP.to(this.$refs.cursor, {
+        x: this.x,
+        y: this.y,
+      })
+    },
+
+    isDisplay(value) {
+      this.$refs.cursor.style.display = value
+    },
+
+    disableCursor() {
+      GSAP.to(this.$refs.cursor, {
+        width: 0,
+        height: 0,
+        opacity: 0,
+        duration: 0.3,
+        onComplete: this.isDisplay('none'),
+      })
+    },
+
+    enableCursor() {
+      GSAP.to(this.$refs.cursor, {
+        width: 100,
+        height: 60,
+        opacity: 1,
+        duration: 0.3,
+        onComplete: this.isDisplay('block'),
+      })
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .section--home-slider {
-  display: flex;
-  transform: translateX(-130px);
-  width: fit-content;
+  &:hover {
+    cursor: grab;
+  }
+
+  &:active {
+    cursor: grabbing;
+  }
+
+  .cursor {
+    background-color: $main-color;
+    width: 100px;
+    height: 60px;
+    position: fixed;
+    top: 40px;
+    left: 40px;
+    transform: translate(-50%, -50%);
+    border-radius: 50%;
+    z-index: 99;
+    text-align: center;
+    align-content: center;
+    display: none;
+
+    @media screen and (min-width: $responsive--mobile-min) and (max-width: $responsive--mobile-max) {
+      display: none !important;
+    }
+
+    @media screen and (min-width: $responsive--tablet-min) and (max-width: $responsive--tablet-max) {
+      display: none !important;
+    }
+
+    span {
+      width: fit-content;
+      position: relative;
+      top: 39%;
+    }
+  }
 
   img {
     width: 500px;
     height: 250px;
     object-fit: cover;
-    margin: 0 30px;
 
     @media screen and (min-width: $responsive--mobile-min) and (max-width: $responsive--mobile-max) {
       width: 300px;

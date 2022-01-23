76a737e2b723924f23cf5d9f1f06595389185bbc
Kanelle Lorent <kanelle.lorent@mail-ecv.fr>
2022-01-23 18:29:16 +0100

    init button component

---
+++ components/Button/Button.vue
@@ -0,0 +1,52 @@
+<template>
+<div :class="{
+  primary: primaryButton,
+  secondary: !primaryButton,
+  }"
+> {{ buttonText }}
+
+</div>
+    
+</template>
+
+<script>
+/* import { ref } from '@vue/reactivity' */
+
+
+
+
+export default {
+  props: {
+
+    primary: {        
+      type: Boolean,
+      default: true,
+    },
+    buttonText: {
+        type: String,
+        default: "Prendre RDV",
+    },
+  },
+  data(){
+    return{
+      primaryButton: this.$props.primary,
+    }
+  },
+}
+</script>
+
+<style scoped>
+div{
+    width: 136px;
+    height: 42px;
+    border: 2px solid;
+    background-color: white;
+    text-align: center;
+}
+.primary{
+  background-color: yellow;
+}
+.secondary{
+  background-color: green;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <Header :defaultStyle="false" />
     <slice-zone type="home" queryType="single" />
+    <Button :primary="false" buttonText="blabla"/>
   </div>
 </template>
 
@@ -9,11 +10,13 @@
 import SliceZone from 'vue-slicezone'
 
 import Header from './../partials/Header/Header.vue'
+import Button from './../components/Button/Button.vue'
 
 export default {
   components: {
     SliceZone,
     Header,
+    Button,
   },
 
   mounted() {

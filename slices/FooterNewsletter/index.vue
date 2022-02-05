3ad469df61a360046d880559d6c90692f4ad0fe4
keshiamukenge <mukenge.keshia@gmail.com>
2022-02-05 14:41:12 +0100

    fix/feature: fixed subheader, display subcat on click, add link animation

---
+++ slices/FooterNewsletter/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <prismic-rich-text :field="slice.primary.newsletterTitle" />
+    <input />
+    <prismic-link :field="slice.primary.InstagramLink">
+      <prismic-rich-text :field="slice.primary.InstagramTitle" />
+    </prismic-link>
+    <prismic-link :field="slice.primary.facebookLink">
+      <prismic-rich-text :field="slice.primary.facebookTitle" />
+    </prismic-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterNewsletter',
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  font-weight: 200;
+
+  input {
+    width: 100%;
+    height: 20px;
+    margin-bottom: 60px;
+  }
+
+  a {
+    color: $white-color;
+    margin-right: 15px;
+  }
+}
+</style>
---
+++ slices/HomeSectionSubPrestas/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div
+      v-for="(item, i) in slice.items"
+      :key="`slice-item-${i}`"
+      :class="`subCat${i}`"
+    >
+      <prismic-link :field="item.linkPrestasSubCat" class="subcat-link">
+        <prismic-rich-text
+          :field="item.textPrestasSubCat"
+          class="subcat-text"
+        />
+      </prismic-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSectionSubPrestas',
+  props: {
+    slice: {
+      type: Object,
+      required: true,
+      default() {
+        return {}
+      },
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  height: fit-content;
+  width: 100%;
+  color: $main-color;
+  margin: 5px 0;
+
+  .subcat-link {
+    overflow: hidden;
+
+    /* .subcat-text {
+      transform: translateY(-20px);
+    } */
+  }
+}
+</style>
